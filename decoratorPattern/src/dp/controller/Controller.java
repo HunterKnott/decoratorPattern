af48bd0b5a631303018347ccde3af94cd6a118de
Hunter Knott <hunterknott00@gmail.com>
2024-02-27 13:39:40 -0700

    Menu and decorator skeletons
    Added menu options after file is entered, and starting to add necessary decorators for StreamOutput

--- decoratorPattern/src/dp/controller/Controller.java
+++ decoratorPattern/src/dp/controller/Controller.java
@@ -3,9 +3,92 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.Writer;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class Controller {
+	private static void printMenu() {
+		System.out.println("Output Decorator Options:\n"
+				+ " (1) Bracket Decorator\n"
+				+ " (2) Numbered Decorator\n"
+				+ " (3) Tee Decorator\n"
+				+ " (4) Filter Decorator\n"
+				+ " (5) Quit\n");
+	}
+	
+	private static void makeDecorator(String type) {
+		if (type.equals("1")) {
+			// BracketOutput: Surrounds each line with square brackets, and adds a newline to each.
+			
+			class BracketOutput extends OutputDecorator {
+
+				public BracketOutput(Writer stream) {
+					super(stream);
+				}
+
+				@Override
+				public void write(Object o) {
+					
+					
+				}
+			}
+		}
+		else if (type.equals("2")) {
+			// NumberedOutput: this precedes each write with the current line number (1-based) right justified
+			// in a field of width 5, followed by a colon and a space. (Don’t add a newline.)
+			
+			class NumberedOutput extends OutputDecorator {
+				
+				public NumberedOutput(Writer stream) {
+					super(stream);
+				}
+				
+				@Override
+				public void write(Object o) {
+					
+				}
+			}
+		}
+		else if (type.equals("3")) {
+			// TeeOutput: writes to two streams at a time; the one it wraps, plus one it receives as a
+			// constructor argument
+			
+			class TeeOutput extends OutputDecorator {
+				
+				public TeeOutput(Writer stream) {
+					super(stream);
+				}
+				
+				@Override
+				public void write(Object o) {
+					
+				}
+			}
+		}
+		else if (type.equals("4")) {
+			// FilterOutput: writes only those objects that satisfy a certain condition (unary predicate),
+			// received as a constructor parameter.
+			
+			class FilterOutput extends OutputDecorator {
+				
+				public FilterOutput(Writer stream) {
+					super(stream);
+				}
+				
+				@Override
+				public void write(Object o) {
+					
+				}
+			}
+		}
+		else {
+			System.out.println("Invalid type");
+			return;
+		}
+	}
+	
 	private static void readFile(InputStream name) {
 		try (BufferedReader reader = new BufferedReader(new InputStreamReader(name))) {
 			String line;
@@ -24,11 +107,31 @@ public class Controller {
 			String fileName = scanner.readLine();
 			
 			InputStream inputStream = Controller.class.getResourceAsStream(fileName);
-			if (inputStream != null) {
+			if (inputStream != null) { // Eventually move this to after the decorators are chosen
 				System.out.println("Reading file " + fileName);
 				readFile(inputStream);
 			} else {
 				System.out.println("No such file named " + fileName);
+				return;
+			}
+			
+			while (true) {
+				printMenu();
+				System.out.print("Select an option: ");
+				String choice = scanner.readLine();
+				
+				List<String> decNumbers = Arrays.asList("1", "2", "3", "4");
+				if (decNumbers.contains(choice)) {
+					System.out.println("Adding Decorator");
+				}
+				else if (choice.equals("5")) {
+					System.out.println("Quitting...");
+					scanner.close();
+					return;
+				}
+				else {
+					System.out.println("Input not valid. Enter a number between 1 and 5");
+				}
 			}
 		} catch (IOException e) {
 			System.err.println("Error reading input: " + e.getMessage());
--- decoratorPattern/src/dp/controller/OutputDecorator.java
+++ decoratorPattern/src/dp/controller/OutputDecorator.java
@@ -2,7 +2,7 @@ package dp.controller;
 
 import java.io.Writer;
 
-public abstract class OutputDecorator extends StreamOutput {
+abstract class OutputDecorator extends StreamOutput {
 	public OutputDecorator(Writer stream) { // Is this the right way to do it?
 		super(stream);
 	}
@@ -10,18 +10,3 @@ public abstract class OutputDecorator extends StreamOutput {
 	StreamOutput so;
 	public abstract void write(Object o);
 }
-
-//1. BracketOutput: Surrounds each line with square brackets, and adds a newline to each.
-
-//2. NumberedOutput: this precedes each write with the current line number (1-based) right justified in a
-// field of width 5, followed by a colon and a space. (Don’t add a newline.)
-
-//3. TeeOutput: writes to two streams at a time; the one it wraps, plus one it receives as a
-//constructor argument
-
-//4. FilterOutput: writes only those objects that satisfy a certain condition (unary predicate),
-//received as a constructor parameter.
-
-//public interface Predicate {
-//	 public boolean execute(Object o); // used by FilterOutput
-//	}
