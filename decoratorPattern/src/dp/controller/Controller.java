84c3d276c16266f801052668140f7f60457e948a
Hunter Knott <hunterknott00@gmail.com>
2024-02-27 11:56:49 -0700

    File read works
    Changed main method and readFile method to work with reading files. Had to use input stream instead of file module

--- decoratorPattern/src/dp/controller/Controller.java
+++ decoratorPattern/src/dp/controller/Controller.java
@@ -1,37 +1,37 @@
 package dp.controller;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
 
 public class Controller {
-	private static void readFile(String name) {
-		File file = new File(name);
-		
-		if (file.exists()) {
-			try {
-				Scanner fileScanner = new Scanner(file);
-				
-				System.out.println("Contents of " + name + ":");
-				while (fileScanner.hasNextLine()) {
-					System.out.println(fileScanner.nextLine());
-				}
-				
-				fileScanner.close();
-			} catch (FileNotFoundException e) {
-				System.err.println("Error reading file: " + e.getMessage());
+	private static void readFile(InputStream name) {
+		try (BufferedReader reader = new BufferedReader(new InputStreamReader(name))) {
+			String line;
+			ArrayList<String> lines = new ArrayList<>();
+			while ((line = reader.readLine()) != null) {
+				System.out.println(line);
 			}
-		} else {
-			System.err.println("File not found: " + name);
+		} catch (IOException e) {
+			System.err.println("Error reading the file: " + e.getMessage());
 		}
 	}
 	
 	public void run() {
-		Scanner scanner = new Scanner(System.in);
-		System.out.print("Enter a file name to read with extension: ");
-		String fileName = scanner.nextLine();
-		System.out.println("You want to read " + fileName);
-		scanner.close();
-		
-		readFile(fileName); // Not working
+		try (BufferedReader scanner = new BufferedReader(new InputStreamReader(System.in))) {
+			System.out.print("Enter a file to read: ");
+			String fileName = scanner.readLine();
+			
+			InputStream inputStream = Controller.class.getResourceAsStream(fileName);
+			if (inputStream != null) {
+				System.out.println("Reading file " + fileName);
+				readFile(inputStream);
+			} else {
+				System.out.println("No such file named " + fileName);
+			}
+		} catch (IOException e) {
+			System.err.println("Error reading input: " + e.getMessage());
+		}
 	}
 }
